ec5a749acbcc56ab6c90696711972d975d223aa8
Jose Lopes <946393361bill@gmail.com>
2021-07-20 16:00:35 +0100

    criando audioElement

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.body.addEventListener('keyup', (event)=>{
-    playSound(event.code)
+    playSound(event.code.toLowerCase())
 });
 
 function playSound(sound) {
-    
-}
+    let audioElement = document.querySelector(`#s_${sound}`)
+};
