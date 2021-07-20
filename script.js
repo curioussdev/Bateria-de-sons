b608473237d431af8df46442a06d671a4307a7c4
Jose Lopes <946393361bill@gmail.com>
2021-07-20 18:19:02 +0100

    escutando evento do input e do button

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ document.body.addEventListener('keyup', (event)=>{
     playSound(event.code.toLowerCase())
 });
 
+document.querySelector(' .composer button').addEventListener('click', ()=>{
+    
+});
+
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
-    
     // selecionar cada elemento pela key
     let keyElement = document.querySelector(`div[data-key="${sound}"]`)
 
@@ -14,16 +17,12 @@ function playSound(sound) {
         audioElement.currentTime = 0;
         audioElement.play()
     }
-
     //verificar se achou o key element
     if(keyElement) {
         keyElement.classList.add('active');
-
-
         // função para remover a class active do elemento (botão) 
         setTimeout(() => {
             keyElement.classList.remove('active');
         }, 300)
     }
-
 };
